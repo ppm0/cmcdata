7d30f663dbf7ecdf5631f880d4d5461369a7ded0
mr <mr@outs.pl>
2019-10-04 20:45:42 +0200

    indexes meta

--- cmc_model.py
+++ cmc_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Numeric, BigInteger, DateTime, Integer, ForeignKey
+from sqlalchemy import Column, String, Numeric, BigInteger, DateTime, Integer, ForeignKey, Index
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -7,11 +7,16 @@ Base = declarative_base()
 class GlobalTokenHistory(Base):
     __tablename__ = 'gth'
     gth_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    ts = Column(DateTime(), nullable = False)
-    tid = Column(Integer, ForeignKey('token.token_id'), nullable = False)
+    ts = Column(DateTime(), nullable=False)
+    tid = Column(Integer, ForeignKey('token.token_id'), nullable=False)
     price_btc = Column(Numeric(30, 8))
     price_usd = Column(Numeric(30, 8))
     volume_24h_usd = Column(Numeric(30, 8))
+
+    __table_args__ = (
+        Index('ix_gth_ts_tid', 'ts', 'tid'),
+        Index('ix_gth_tid_ts', 'tid', 'ts'),
+    )
 
 
 class Token(Base):
